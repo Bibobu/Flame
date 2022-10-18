2c4818bcf272b36215bafb673c28844f737d2b10
Germain Clavier <germain.clavier@gmail.com>
2022-10-18 15:38:07 +0200

    Better parsers

--- src/tools/parsers.rs
+++ src/tools/parsers.rs
@@ -1,6 +1,11 @@
+use std::str::FromStr;
 use nom::IResult;
-use nom::branch::alt;
-use nom::bytes::complete::tag;
+// use nom::branch::alt;
+use nom::character::complete::{not_line_ending, digit1};
+use nom::number::complete::double;
+use nom::bytes::complete::{tag, take_until};
+use nom::multi::many_m_n;
+use nom::combinator::map_res;
 
 // Parser related functions
 
@@ -8,6 +13,28 @@ use nom::bytes::complete::tag;
 // Format spec should be dealt with in lmpat.rs
 pub fn atom_parser(s: &str) -> IResult<&str, Vec<&str>> {
     nom::multi::many0(nom::bytes::complete::tag(" "))(s)
+}
+
+fn parse_integer(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, u32::from_str)(input.trim_start())
+}
+
+fn parse_float(input: &str) -> IResult<&str, f64> {
+    double(input.trim_start())
+}
+
+// Parses header num + field lines
+pub fn head_field_parser(s: &str) -> IResult<&str, Vec<u32>> {
+    many_m_n(1, 2, parse_integer)(s)
+}
+
+pub fn box_field_parser(s: &str) -> IResult<&str, Vec<f64>> {
+    many_m_n(2, 3, parse_float)(s)
+}
+
+// Parses each line of the input
+pub fn line_parser(s: &str) -> IResult<&str, &str> {
+    not_line_ending(s)
 }
 
 // Should split the file at the first body keyword encountered
